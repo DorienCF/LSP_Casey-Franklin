1f0826587a4527675fef6875d08d1c44e1e0a48f
Dorien CF <doriencasey@gmail.com>
2022-01-28 11:59:58 -0500

    Create SimpleWordProcessor.java
    Base Commit.

---
+++ LargeScalePrograming/src/org/howard/edu/lsp/assignment2/SimpleWordProcessor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment2;
+
+/**
+ * @author doriencasey-franklin
+ *
+ */
+public class SimpleWordProcessor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("String?");
+	}
+
+}
