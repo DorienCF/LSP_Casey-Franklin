4ceda9e98632c6e139e84991ec427a182aa253f3
Dorien CF <doriencasey@gmail.com>
2022-02-06 23:38:03 -0500

    final update

--- LargeScalePrograming/src/org/howard/edu/lsp/assignment2/SimpleWordProcessor.java
+++ LargeScalePrograming/src/org/howard/edu/lsp/assignment2/SimpleWordProcessor.java
@@ -2,6 +2,7 @@
  * 
  */
 package org.howard.edu.lsp.assignment2;
+import java.util.Scanner;
 
 /**
  * @author doriencasey-franklin
@@ -13,8 +14,42 @@ public class SimpleWordProcessor {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("String?");
-	}
 
+while(true) {
+	 // User input
+		Scanner input = new Scanner(System.in); // Sets var input to scan for input
+		System.out.print("String? \n");
+		 String usrInput = input.nextLine();
+		if (usrInput == "Goodbye") {
+			System.exit(0);
+		}
+
+		
+	// String to string array 
+	      String[] ch = null;
+	      ch = usrInput.split((" "));
+	      System.out.println("Tokens: ");
+	      for (int i = 0; i < ch.length; i++) {
+	         System.out.print(ch[i]+"\n"); 
+	      }
+	      
+	      // addition
+	      int sum = 0;
+	      for (int i=0; i < ch.length; i++) {
+	    			  int element = Integer.parseInt(String.valueOf(ch[i]));
+	    	  		  sum = element + sum;
+	      }
+	      System.out.println("Sum: " + sum);
+	      
+	      // multiplication
+	      int product = 1;
+	      for (int i=0; i < ch.length; i++) {
+	    			  int element = Integer.parseInt(String.valueOf(ch[i]));
+	    	  		  product = element * product;
+	    	  
+	      }
+	      System.out.println("Product: " + product);
+
+	}
+}
 }
