2e5c10be0938ff052cd43863a7b3eabf3359d66b
Dorien CF <doriencasey@gmail.com>
2022-03-05 22:01:04 -0500

    Assignment5
    Final Push

--- LargeScalePrograming/src/org/howard/edu/lsp/assignment5/implementation/IntegerSet.java
+++ LargeScalePrograming/src/org/howard/edu/lsp/assignment5/implementation/IntegerSet.java
@@ -1,123 +1,136 @@
 package org.howard.edu.lsp.assignment5.implementation;
-
 //import java.rmi.Remote;
-import java.util.ArrayList;
-//import java.util.Set;
-import java.util.Set;
+	import java.util.ArrayList;
+	//import java.util.Set;
+	//import java.util.Set;
 
-public class IntegerSet {
-   //private Set<Integer> = set new ArrayList<integer>; // from doc
-    private ArrayList<Integer> Set = new ArrayList<>();
+	public class IntegerSet {
+	   //private Set<Integer> = set new ArrayList<integer>; // from doc
+	    private ArrayList<Integer> Set = new ArrayList<>();
 
-   public IntegerSet()
-   {
+	 //
+	    public class IntegerSetException  extends Exception
+	    {
+	        public IntegerSetException (String s)
+	        {
+	            // Call constructor of parent Exception
+	            super(s);
+	        }
+	    }
+	     
+	    
+	
+	       public void clear() {
+	           Set.clear();
+	    
+	       }
+	       // fetches the  lenght  of the set
+	       public int length() {
+	        int set_length = Set.size();
+	        return set_length;
+	           
+	       }
+	       // equals: Complete
+	       public boolean equals(IntegerSet IntSetB){ // doc says equals(Integeer Set b)
+	           if(length() == IntSetB.length())
+	           {
+	               return false;
+	           }
+	           for(int i = 0; i < length(); i++)
+	           {
+	                if(!IntSetB.contains(Set.get(i)))
+	                {
+	                    return false;
+	                }
+	           }
+	           return true;
+	       }
+	       // contains: Complete
+	       public boolean contains(int value) {
+	        for(int i = 0; i < length(); i++){
+	            if(value == Set.get(i)){
+	                return true;
+	            }
+	        }
+	        return false;
+	       }
+	       //largest: complete
+	       public int largest() throws IntegerSetException{ // Going to throw a  IntergersetException per  doc if set is empty 
+	        if(this.length() == 0){
+	            throw new IntegerSetException("Set is empty"); // return an error ... 
+	        }
+	        int largest = Set.get(0);
+	        for(int i = 0; i < length(); i++){
+	            if(largest < Set.get(i)){
+	                largest = Set.get(i);
+	            }
+	        }
+	        return largest;
+	       }
+	       // smallest: complete
+	       public int smallest() throws IntegerSetException { // Throws an IntergerSetExecption if set is empmty per doc
+	        if(this.length() == 0){
+	            throw new IntegerSetException("Set is empty"); // return an error ... 
+	        }
+	        int smallest = Set.get(0);
+	        for(int i = 0; i < length(); i++){
+	            if(smallest > Set.get(i)){
+	                smallest = Set.get(i);
+	            }
+	        }
+	        return smallest;
+	       }
+	       // add: Complete
+	       public void add(int value) {
+	           if(!contains(value)){
+	            Set.add(value);
+	           }
+	       }
+	       // remove: complete
+	       public void remove(int value) throws IntegerSetException {
+	    	   if (this.length()==0) {
+	    		   throw new IntegerSetException("Set is Empty");
+	    	   }
+	        Set.remove(value);
+	       }
+	       //Union
+	       public void union(IntegerSet intSetb){
+	           
+	        for(int i = 0; i < length(); i++){
+	            if(!intSetb.contains(Set.get(i)))
+	            {
+	                Set.add(i);
+	            }
+	        }
+	       }
+	       //Intersect: 
+	       public void intersect(IntegerSet intSetb){
+	        for(int i = 0; i < length(); i++){
+	            if(!intSetb.contains(Set.get(i)))
+	            {
+	                Set.remove(i);
+	            }
+	        }
+	       }
+	       public void difference(IntegerSet intSetb){
 
-   }
-// clears sets
-       public void clear() {
-           Set.clear();
-    
-       }
-       // fetches the  lenght  of the set
-       public int length() {
-        int set_length = Set.size();
-        return set_length;
-           
-       }
-       
-       public boolean equals(IntegerSet IntSetB){ // doc says equals(Integeer Set b)
-           if(length() == IntSetB.length())
-           {
-               return false;
-           }
-           for(int i = 0; i < length(); i++)
-           {
-                if(!IntSetB.contains(Set.get(i)))
-                {
-                    return false;
-                }
-           }
-           return true;
-       }
-       public boolean contains(int value) {
-        for(int i = 0; i < length(); i++){
-            if(value == Set.get(i)){
-                return true;
-            }
-        }
-        return false;
-       }
+	       }
+	       //IsEmpty Complete:
+	       public boolean IsEmpty(){
+	           boolean EmptyCheck = Set.isEmpty();
+	           return EmptyCheck;
+	       }
+	       // SetToString :Complete
+	       public String SetToString(){
+	           String StringSet  = Set.toString();
+	           return StringSet;
 
-       public int largest(){ // Going to throw a  IntergersetException per  doc if set is empty 
-        if(this.length() == 0){
-            return 0; // return an error ... 
-        }
-        int largest = Set.get(0);
-        for(int i = 0; i < length(); i++){
-            if(largest < Set.get(i)){
-                largest = Set.get(i);
-            }
-        }
-        return largest;
-       }
-       public int smallest(){ // Throws an IntergerSetExecption if set is empmty per doc
-        if(this.length() == 0){
-            return 0; // return an error ... 
-        }
-        int smallest = Set.get(0);
-        for(int i = 0; i < length(); i++){
-            if(smallest > Set.get(i)){
-                smallest = Set.get(i);
-            }
-        }
-        return smallest;
-       }
-       public void add(int value) {
-           if(!contains(value)){
-            Set.add(value);
-           }
-       }
-       public void remove(int value){
-        Set.remove(value);
-       }
-       public void union(IntegerSet intSetb){
-           
-        for(int i = 0; i < length(); i++){
-            if(!intSetb.contains(Set.get(i)))
-            {
-                Set.add();
-            }
-        }
-       }
-       public void intersect(IntegerSet intSetb){
-        for(int i = 0; i < length(); i++){
-            if(!intSetb.contains(Set.get(i)))
-            {
-                Set.remove(i);
-            }
-        }
-       }
-       public void difference(IntegerSet intSetb){
-
-       }
-       public boolean IsEmpty(){
-           boolean EmptyCheck = Set.isEmpty();
-           return EmptyCheck;
-       }
-       public String SetToString(){
-           String StringSet  = Set.toString();
-           return StringSet;
-
-       }
-
-  
-    };
+	       }
+		
+	  
+	    };
 
 
 
 
-///* 1) Set a private "variable" to  create what a set is 
-//* 2.) use the methods native to ArrayList to do Add & remove
-//* 3.) use lenght method to determine  the lenghts...duh and use that method innternally for the iteration lenght of loopss
-//*
-///*
+
--- LargeScalePrograming/src/org/howard/edu/lsp/assignment5/test/Driver.java
+++ LargeScalePrograming/src/org/howard/edu/lsp/assignment5/test/Driver.java
@@ -1,5 +1,79 @@
 package org.howard.edu.lsp.assignment5.test;
 
+import org.howard.edu.lsp.assignment5.implementation.IntegerSet;
+import org.howard.edu.lsp.assignment5.implementation.IntegerSet.IntegerSetException;
+import org.w3c.dom.events.EventException;
+
 public class Driver {
+	public static void main(String[] args) {
+		
+	IntegerSet SetA = new  IntegerSet();
+	
+	//Adding elements to set A:
+	SetA.add(0);
+	SetA.add(1);
+	SetA.add(2);
+	SetA.add(3);
+	
+	IntegerSet SetB = new IntegerSet();
+	// Adding elements to Set B
+	SetB.add(4);
+	SetB.add(5);
+	SetB.add(6);
+	SetB.add(7);
+	// Print out the  other functions to so the program is operational:
+	
+		// Printing out each set
+		System.out.println("Set A: " + SetA.SetToString());
+		System.out.println("Set B: " + SetB.SetToString());
+		
+		// Printing if each set is equal
+		System.out.println("Are sets A and B equal: " + SetA.equals(SetB));
+		
+		// Smallest of each set
+		try {
+			System.out.println("The smallest of set A: " + SetA.smallest());
+
+		System.out.println("The smallest of set B: " + SetB.smallest());
+		
+		//Largest of each set
+		System.out.println("The Largest of Set A: " + SetA.largest());
+		System.out.println("The Largest of Set B: " + SetB.largest());
+		SetA.remove(0);
+		} catch(IntegerSetException ex) {
+			
+            System.out.println("Execption:");
+            System.out.println(ex.getMessage());
+		}
+		// Removing 0 and adding 4 to  set a
+		
+		SetA.add(4);
+		System.out.println("New Set A: " + SetA.SetToString());
+		
+		//testing contains
+		System.out.println("Does Set A contain '4'?: " + SetA.contains(4));
+		
+		//testing if the sets are empty
+		System.out.println("Set A is empty: " + SetA.IsEmpty());
+		
+		//testing Union
+		SetA.union(SetB);
+		System.out.println("Union of A & B: " + SetA.SetToString());
+		
+		
+		
+		//testing clear
+		SetB.clear();
+		System.out.println("SetA: "+SetA.SetToString());
+		// re-adding values:
+		SetA.add(0);
+		SetA.add(1);
+		SetA.add(2);
+		SetA.add(3);
+		//testing intersection
+		SetA.intersect(SetB);
+		System.out.println("Intersection of Set A and B: " + SetA.SetToString());
+	
+	}
 
 }
